a41a8b4f461dc72a3da26082afb55d9f3ef052c6
Boitumelo <boityy.js@gmail.com>
2023-10-30 13:52:47 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./logo192.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="App-logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
