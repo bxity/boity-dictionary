48f684c36116b1ea8085bc031d913c14a947ce5f
Boitumelo <boityy.js@gmail.com>
2023-10-30 14:57:37 +0200

    added form for the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import logo from "./logo192.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} alt="logo" className="App-logo" />
       </header>
+      <main>
+       <Dictionary />
+
+      </main>
+      <footer>
+        Coded by <a href="https://github.com/bxity">Boitumelo Vilakazi</a> and
+        is <a href="https://github.com/bxity/boity-dictionary">open-sourced on GitHub</a>
+      </footer>
+    </div>
     </div>
   );
 }
