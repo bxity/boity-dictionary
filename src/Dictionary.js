86c92f381e753d196638b23e2f04fe944f2418d6
Boitumelo <boityy.js@gmail.com>
2023-10-30 15:54:44 +0200

    added working results from API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
-     let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
-     function handleResponse(response) {
+  function search(event) {
+    event.preventDefault();
 
-     }
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function search(event) {
-        event.preventDefault();
-        
-        let apiKey = "3a5627ae326058co2dc72a44fbta8eef";
-        let apiUrl =
-          `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key={apiKey}`;
-          axios.get(apiUrl).then(handleResponse);
-    }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-
-
-
-     function handleKeywordChange(event) {
-      setKeyword(event.target.value);
-    }
-
-     return (
-        <div className="Dictionary">
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} autoFocus={true}/>
-            </form>
-        </div>
-     );
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} autoFocus={true} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
