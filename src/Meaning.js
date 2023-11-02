51b459c1e18d7db06b4cedea6a8bdcfd325935c2
Boitumelo <boityy.js@gmail.com>
2023-11-02 12:41:11 +0200

    fixed syntax errors affecting the structure

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,10 +9,10 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition :</strong> {definition.definition}
+              <strong>Definition: </strong> {definition.definition}
               <br />
-              <strong>Example :</strong> <em>{definition.example}</em>
-              <br />
+              <strong>Example: </strong> 
+              <em>{definition.example}</em>
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
