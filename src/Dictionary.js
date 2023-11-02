ae32faea078ed6dd0e6f79baebb0ea0992258f8c
Boitumelo <boityy.js@gmail.com>
2023-11-02 15:53:53 +0200

    added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,28 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
+
+  function handlePexelsResponse(response) {
+    console.log(response);
+  }
+
+
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+
+    let pexelsApiKey =
+      "7C49yoUXi37d0dvdL1SDg6gskM6ZZaPtU7U92vWDshlVDdaZZ1WGPIFq";
+      let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+      axios.get(pexelsApiUrl).then(handlePexelsResponse);
+
+
   }
 
   function handleSubmit(event) {
